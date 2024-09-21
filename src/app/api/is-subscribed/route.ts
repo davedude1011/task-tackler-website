c5ba8a93d0de4a4790e37c2385932f7d6d82e89d
davedude1011 <tom2smallwood@gmail.com>
2024-09-21 11:47:36 +0100

    fixed is-subscribed api

--- src/app/api/is-subscribed/route.ts
+++ src/app/api/is-subscribed/route.ts
@@ -2,20 +2,24 @@ import { NextResponse } from 'next/server';
 import { getSubscriptions } from '~/server/subscriptions';
 import { isSubscriptionByPassed } from '~/server/userData';
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type',
   };
-  const stripeId = await req.text();
-  try {
-    const subscription = await getSubscriptions(stripeId)
-    const subscriptionByPass = await isSubscriptionByPassed(stripeId)
-    return NextResponse.json(JSON.stringify({
-      isSubscribed: subscription.length > 0 || subscriptionByPass
-    }), {headers});
-  } catch (error) {
-    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
+  if (req.method === 'OPTIONS') {
+    return new NextResponse(null, { headers });
   }
+
+  const stripeId = await req.text().then(id => id.replace(/"/g, '').trim());
+
+  const subscription = await getSubscriptions(stripeId)
+  const isSubscribed = subscription.length > 0
+  const subscriptionByPass = await isSubscriptionByPassed(stripeId)
+  
+  return new NextResponse(
+    JSON.stringify({ isSubscribed: (isSubscribed || subscriptionByPass) }),
+    { headers }
+  );
 }
