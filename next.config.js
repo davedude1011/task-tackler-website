64726ca64b458af920b6852b0a3123879412996e
davedude101 <davedude.production.inc@gmail.com>
2025-04-17 17:29:31 +0100

    not strict

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
 
 export default config;
